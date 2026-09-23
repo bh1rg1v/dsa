1e84e910dd0481b99c77dfa03c44e93b1f9fce7c
bh1rg1v <manukondabhargavram@gmail.com>
2026-09-24 01:58:29 +0530

    Add Climbing-Stairs (dynamic-programming)

---
+++ leetcode/dynamic-programming/70-Climbing-Stairs.py
@@ -0,0 +1,11 @@
+dp = [0] * 50
+
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, 50):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        return dp[n]
